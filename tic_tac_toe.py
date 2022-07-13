c0742669ab73103f62e9f3518e40250316cecd36
Yolanda <yolmabot021@student.wethinkcode.co.za>
2022-07-13 13:00:13 +0200

    added the menu

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -26,6 +26,28 @@ def print_scoreboard(score_board):
  
     print("--------------------------------\n")
 
+    # Function to check if any player has won
+def check_win(player_pos, cur_player):
+ 
+    # All possible winning combinations
+    soln = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
+ 
+    # Loop to check if any winning combination is satisfied
+    for x in soln:
+        if all(y in player_pos[cur_player] for y in x):
+ 
+            # Return True if any winning combination satisfies
+            return True
+    # Return False if no combination is satisfied       
+    return False 
+
+    # Function to check if the game is drawn
+def check_draw(player_pos):
+    if len(player_pos['X']) + len(player_pos['O']) == 9:
+        return True
+    return False   
+          
+
 # Function for a single game of Tic Tac Toe
 def single_game(cur_player):
  
@@ -49,12 +71,7 @@ def single_game(cur_player):
     while True:
         print_squre_grid(values)
         # Try exception block for MOVE input
-        try:
-            print("Player ", cur_player, " turn. Which box? : ", end="")
-            move = int(input()) 
-        except ValueError:
-            print("Wrong Input!!! Try Again")
-            continue
+        
         
         # Sanity check for MOVE inout
         if move < 1 or move > 9:
@@ -75,41 +92,6 @@ def single_game(cur_player):
 
         # Function call for checking win
     
-    
-   
-# Function to check if any player has won
-def check_win(player_pos, cur_player):
- 
-    # All possible winning combinations
-    soln = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
- 
-    # Loop to check if any winning combination is satisfied
-    for x in soln:
-        if all(y in player_pos[cur_player] for y in x):
- 
-            # Return True if any winning combination satisfies
-            return True
-    # Return False if no combination is satisfied       
-    return False       
- 
-# Function to check if the game is drawn
-def check_draw(player_pos):
-    if len(player_pos['X']) + len(player_pos['O']) == 9:
-        return True
-    return False   
-    
-
-
-    # Game Loop for a series of Tic Tac Toe
-    # The loop runs until the players quit 
-    while True:
-    
-        # Player choice Menu
-        print("Turn to choose for", cur_player)
-        print("Enter 1 for X")
-        print("Enter 2 for O")
-        print("Enter 3 to Quit")
-
 if __name__ == "__main__":
 
     print("Player 1")
@@ -139,3 +121,20 @@ if __name__ == "__main__":
     # Stores the scoreboard
     score_board = {player1: 0, player2: 0}
     print_scoreboard(score_board)
+
+    # Game Loop for a series of Tic Tac Toe
+    # The loop runs until the players quit 
+    while True:
+    
+        # Player choice Menu
+        print("Turn to choose for", cur_player)
+        print("Enter 1 for X")
+        print("Enter 2 for O")
+        print("Enter 3 to Quit")
+
+        try:
+            print("Player ", cur_player, " turn. Which box? : ", end="")
+            move = int(input()) 
+        except ValueError:
+            print("Wrong Input!!! Try Again")
+            continue
